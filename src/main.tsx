338fa2cb24c9555b8dfd4fcefc9c22f88a7f4920
Gabriel Garcia <garciat@uber.com>
2024-12-29 00:30:09 +0100

    add footer

--- src/main.tsx
+++ src/main.tsx
@@ -54,82 +54,98 @@ const App = ({}: AppProps) => {
   }
 
   return (
-    <main>
-      <h1>wgpu-memory</h1>
-      <p>
-        <a href="https://github.com/Garciat/wgpu-memory">
-          <img
-            alt="Static Badge"
-            src="https://img.shields.io/badge/GitHub-source-blue?logo=github"
-          />
-        </a>{" "}
-        <a href="https://jsr.io/@garciat/wgpu-memory">
-          <img src="https://jsr.io/badges/@garciat/wgpu-memory" />
-        </a>{" "}
-        <a href="https://jsr.io/@garciat/wgpu-memory">
-          <img src="https://jsr.io/badges/@garciat/wgpu-memory/score" />
-        </a>{" "}
-        <a href="https://github.com/garciat/wgpu-memory">
-          <img src="https://github.com/garciat/wgpu-memory/workflows/ci/badge.svg" />
-        </a>{" "}
-        <a href="https://codecov.io/gh/garciat/wgpu-memory">
-          <img src="https://codecov.io/gh/garciat/wgpu-memory/branch/main/graph/badge.svg?token=KEKZ52NXGP" />
-        </a>
-      </p>
-      <p>
-        A utility library for WebGPU that provides strongly-typed{" "}
-        <code>ArrayBuffer</code> memory access that is compatible with{" "}
-        <a href="https://gpuweb.github.io/gpuweb/wgsl/">
-          WGSL
-        </a>'s{" "}
-        <a href="https://gpuweb.github.io/gpuweb/wgsl/#alignment-and-size">
-          alignment and size specifications
-        </a>.
-      </p>
-      <p>Below you may use a visual editor for the supported memory types.</p>
-      <h2>Memory Type</h2>
-      <p class="warning" hidden={"Float16Array" in globalThis}>
-        <strong>Note:</strong> Your JavaScript environment does not support{" "}
-        <a href="https://tc39.es/proposal-float16array/">Float 16</a>. If you
-        use <code>f16</code> below, then the page will stop working.
-      </p>
-      <section class="centered">
-        <MemoryTypeEditor type={memoryType} onChange={onMemoryTypeChange} />
-      </section>
-      <section class="centered">
-        <pre class="syntax-highlight">{
+    <>
+      <main>
+        <h1>
+          wgpu-memory{" "}
+          <sub>
+            by <a href="https://github.com/Garciat">@garciat</a>
+          </sub>
+        </h1>
+        <p>
+          <a href="https://github.com/Garciat/wgpu-memory">
+            <img
+              alt="Static Badge"
+              src="https://img.shields.io/badge/GitHub-source-blue?logo=github"
+            />
+          </a>{" "}
+          <a href="https://jsr.io/@garciat/wgpu-memory">
+            <img src="https://jsr.io/badges/@garciat/wgpu-memory" />
+          </a>{" "}
+          <a href="https://jsr.io/@garciat/wgpu-memory">
+            <img src="https://jsr.io/badges/@garciat/wgpu-memory/score" />
+          </a>{" "}
+          <a href="https://github.com/garciat/wgpu-memory">
+            <img src="https://github.com/garciat/wgpu-memory/workflows/ci/badge.svg" />
+          </a>{" "}
+          <a href="https://codecov.io/gh/garciat/wgpu-memory">
+            <img src="https://codecov.io/gh/garciat/wgpu-memory/branch/main/graph/badge.svg?token=KEKZ52NXGP" />
+          </a>
+        </p>
+        <p>
+          A utility library for WebGPU that provides strongly-typed{" "}
+          <code>ArrayBuffer</code> memory access that is compatible with{" "}
+          <a href="https://gpuweb.github.io/gpuweb/wgsl/">
+            WGSL
+          </a>'s{" "}
+          <a href="https://gpuweb.github.io/gpuweb/wgsl/#alignment-and-size">
+            alignment and size specifications
+          </a>.
+        </p>
+        <p>Below you may use a visual editor for the supported memory types.</p>
+        <h2>Memory Type</h2>
+        <p class="warning" hidden={"Float16Array" in globalThis}>
+          <strong>Note:</strong> Your JavaScript environment does not support
+          {" "}
+          <a href="https://tc39.es/proposal-float16array/">Float 16</a>. If you
+          use <code>f16</code> below, then the page will stop working.
+        </p>
+        <section class="centered">
+          <MemoryTypeEditor type={memoryType} onChange={onMemoryTypeChange} />
+        </section>
+        <section class="centered">
+          <pre class="syntax-highlight">{
           JSON.stringify({
             byteSize: memoryType.byteSize,
             alignment: memoryType.alignment,
             arrayStride: memoryType.arrayStride,
           })
         }</pre>
-      </section>
-      <h2>WGSL (approximate)</h2>
-      <section class="centered">
-        <pre class="syntax-highlight">{String(memoryType)}</pre>
-      </section>
-      <h2>JavaScript</h2>
-      <section class="centered">
-        <pre class="syntax-highlight">
+        </section>
+        <h2>WGSL (approximate)</h2>
+        <section class="centered">
+          <pre class="syntax-highlight">{String(memoryType)}</pre>
+        </section>
+        <h2>JavaScript</h2>
+        <section class="centered">
+          <pre class="syntax-highlight">
           {`import * as memory from "jsr:@garciat/wgpu-memory@1.0.14";\n\n`}
           {`const myType = ${memoryType.toCode("memory")};`}
-        </pre>
-      </section>
-      <h2>Object Value</h2>
-      <section class="centered object-value-container">
-        <MemoryValueEditor
-          type={memoryType}
-          buffer={bytes.buffer}
-          offset={0}
-          onChange={onMemoryValueChange}
-        />
-      </section>
-      <h2>Memory View</h2>
-      <section class="centered">
-        <BinaryDumpTable bytes={bytes} />
-      </section>
-    </main>
+          </pre>
+        </section>
+        <h2>Object Value</h2>
+        <section class="centered object-value-container">
+          <MemoryValueEditor
+            type={memoryType}
+            buffer={bytes.buffer}
+            offset={0}
+            onChange={onMemoryValueChange}
+          />
+        </section>
+        <h2>Memory View</h2>
+        <section class="centered">
+          <BinaryDumpTable bytes={bytes} />
+        </section>
+      </main>
+      <footer>
+        <p>
+          Built with <a href="https://www.typescriptlang.org/">TypeScript</a>,
+          {" "}
+          <a href="https://lume.land">Lume</a>, and{" "}
+          <a href="https://preactjs.com">Preact</a>.
+        </p>
+      </footer>
+    </>
   );
 };
 
